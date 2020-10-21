0c97314ec81282375fb02e995e7d43808689d682
tjenkins <torii.j.jenkins@gmail.com>
2020-10-20 20:58:56 -0400

    User and Post classes added

---
+++ Sociality.Data/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociality.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public User Author { get; set; }
+    }
+}
---
+++ Sociality.Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociality.Data
+{
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string  Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
