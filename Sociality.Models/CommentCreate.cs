bb5b2ad0735d8cb5105584da9eb38115c7ad215a
sellersjt <jonnie.sellers@gmail.com>
2020-10-21 17:17:52 -0400

    add comment models

---
+++ Sociality.Models/CommentCreate.cs
@@ -0,0 +1,22 @@
+﻿using Sociality.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociality.Models
+{
+    public class CommentCreate
+    {
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public User Author { get; set; }
+
+        [Required]
+        public Post CommentPost { get; set; }
+    }
+}
