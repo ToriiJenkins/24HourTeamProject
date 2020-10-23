b41cebf53d67d94c222e6d5551d448cac3efddc4
tjenkins <torii.j.jenkins@gmail.com>
2020-10-23 19:05:56 -0400

    Added Jonnies Models and Services

---
+++ Sociality.Models/LikeEdit.cs
@@ -0,0 +1,15 @@
+﻿using Sociality.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociality.Models
+{
+    public class LikeEdit
+    {
+        public Post LikedPost { get; set; }
+        public User Liker { get; set; }
+    }
+}
---
+++ Sociality.Models/ReplyListItem.cs
@@ -0,0 +1,18 @@
+﻿using Sociality.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociality.Models
+{
+    public class ReplyListItem
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public User Author { get; set; }
+        public Post CommentPost { get; set; }
+        public Comment ReplyComment { get; set; }
+    }
+}
